c5fec740a00c8fbea7d9ad3b9b25125e8509c6e7
Khaled-Mohammed-Abdelgaber <66785999+Khaled-Mohammed-Abdelgaber@users.noreply.github.com>
2022-07-13 21:23:35 +0200

    Create heart_rate_equqlization.py

---
+++ heart_rate_equqlization.py
@@ -0,0 +1,12 @@
+def heart_rate_equqlization(data_frame,n_signals = 10001,p_threshold = 0.7, rate = 9):
+  eq_rate_data = pd.DataFrame()
+  for i in range(n_signals):
+    print(i)
+    peaks, _ = find_peaks(data_frame.loc[i], height=p_threshold)
+    if len(peaks) < rate:
+      continue
+    arr = data_frame.loc[i].values[:peaks[rate - 1]]
+    scaled = rescale(arr, factor= 1024/peaks[rate - 1])
+    df = pd.DataFrame(data = scaled )
+    eq_rate_data = pd.concat([eq_rate_data , df],axis = 1)
+  return eq_rate_data
